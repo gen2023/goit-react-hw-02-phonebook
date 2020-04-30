a08f692d8d86be966fd2c10cff0970ed4dffcf59
Евгений <evgenij.romanko@neoseo.com.ua>
2020-04-30 22:05:41 +0300

    Updates

---
+++ precache-manifest.9ea01f8a2171340b7c8a5a6f8ab306eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d83201691d2c5e7f10558cf7b1c2aa",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "28860a60920f3ee8c192",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.9fc4e42e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.9fc4e42e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "634243c5b46d99e34c1e",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.b5a6f2e9.chunk.js"
+  },
+  {
+    "revision": "bfccad9aa0892efebfa9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.98a8e906.js"
+  }
+]);
